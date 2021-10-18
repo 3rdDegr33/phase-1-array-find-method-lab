45c0e48bd2462e403d6581ce7ae151abe8c3d0e6
Johnny Moya <profjmoya@gmail.com>
2021-10-18 18:36:02 -0500

    commit tree 6366874d0a2024ee45cc9fc99ff9cec439ab7795

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record) {
+    const found = record.find(element => element.result === "W");
+    
+    if(found) {
+        return found.year;
+    } else {
+        return undefined;
+    }
+}
